611d66dfb4eb9122ad7f2f4df639eb7fa2f8e941
Jiewen Yao <jiewen.yao@intel.com>
2023-01-22 13:50:02 +0800

    test_crypt for tongsuo.
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- unit_test/test_crypt/sm2_verify.c
+++ unit_test/test_crypt/sm2_verify.c
@@ -20,12 +20,12 @@
 bool libspdm_validate_crypt_sm2(void)
 {
     void *Sm2_1;
-    void *Sm2_2;
     uint8_t public1[66 * 2];
     size_t public1_length;
+    #if LIBSPDM_SM2_KEY_EXCHANGE_SUPPORT_TEST
+    void *Sm2_2;
     uint8_t public2[66 * 2];
     size_t public2_length;
-    #if LIBSPDM_SM2_KEY_EXCHANGE_SUPPORT_TEST
     uint8_t key1[66];
     size_t key1_length;
     uint8_t key2[66];
@@ -188,85 +188,6 @@ bool libspdm_validate_crypt_sm2(void)
     }
     libspdm_sm2_dsa_free(Sm2_1);
 
-    libspdm_my_print("\nCrypto sm2 Signing Verification Testing with SetPubKey:\n");
-
-    public1_length = sizeof(public1);
-    public2_length = sizeof(public2);
-
-    libspdm_my_print("- Context1 ... ");
-    Sm2_1 = libspdm_sm2_dsa_new_by_nid(LIBSPDM_CRYPTO_NID_SM2_DSA_P256);
-    if (Sm2_1 == NULL) {
-        libspdm_my_print("[Fail]");
-        return false;
-    }
-
-    libspdm_my_print("Context2 ... ");
-    Sm2_2 = libspdm_sm2_dsa_new_by_nid(LIBSPDM_CRYPTO_NID_SM2_DSA_P256);
-    if (Sm2_2 == NULL) {
-        libspdm_my_print("[Fail]");
-        libspdm_sm2_dsa_free(Sm2_1);
-        return false;
-    }
-
-    libspdm_my_print("Compute key in Context1 ... ");
-    status = libspdm_sm2_dsa_generate_key(Sm2_1, public1, &public1_length);
-    if (!status) {
-        libspdm_my_print("[Fail]");
-        libspdm_sm2_dsa_free(Sm2_1);
-        libspdm_sm2_dsa_free(Sm2_2);
-        return false;
-    }
-
-    libspdm_my_print("Export key in Context1 ... ");
-    status = libspdm_sm2_dsa_get_pub_key(Sm2_1, public2, &public2_length);
-    if (!status) {
-        libspdm_my_print("[Fail]");
-        libspdm_sm2_dsa_free(Sm2_1);
-        libspdm_sm2_dsa_free(Sm2_2);
-        return false;
-    }
-
-    libspdm_my_print("Import key in Context2 ... ");
-    status = libspdm_sm2_dsa_set_pub_key(Sm2_2, public2, public2_length);
-    if (!status) {
-        libspdm_my_print("[Fail]");
-        libspdm_sm2_dsa_free(Sm2_1);
-        libspdm_sm2_dsa_free(Sm2_2);
-        return false;
-    }
-
-    /* Verify EC-DSA*/
-    sig_size = sizeof(signature);
-    libspdm_my_print("\n- sm2 Signing in Context1 ... ");
-    status =
-        libspdm_sm2_dsa_sign(Sm2_1, LIBSPDM_CRYPTO_NID_SM3_256, (uint8_t *)DEFAULT_SM2_ID,
-                             sizeof(DEFAULT_SM2_ID) - 1,
-                             message,
-                             sizeof(message), signature, &sig_size);
-    if (!status) {
-        libspdm_my_print("[Fail]");
-        libspdm_sm2_dsa_free(Sm2_1);
-        libspdm_sm2_dsa_free(Sm2_2);
-        return false;
-    }
-
-    libspdm_my_print("sm2 Verification in Context2 ... ");
-    status =
-        libspdm_sm2_dsa_verify(Sm2_2, LIBSPDM_CRYPTO_NID_SM3_256, (uint8_t *)DEFAULT_SM2_ID,
-                               sizeof(DEFAULT_SM2_ID) - 1,
-                               message,
-                               sizeof(message), signature, sig_size);
-    if (!status) {
-        libspdm_my_print("[Fail]");
-        libspdm_sm2_dsa_free(Sm2_1);
-        libspdm_sm2_dsa_free(Sm2_2);
-        return false;
-    } else {
-        libspdm_my_print("[Pass]\n");
-    }
-
-    libspdm_sm2_dsa_free(Sm2_1);
-    libspdm_sm2_dsa_free(Sm2_2);
     #endif /* LIBSPDM_SM2_DSA_SUPPORT_TEST */
 
     return true;
